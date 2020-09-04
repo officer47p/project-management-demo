85ed2a593d7e161d1c2de04ad32563723a4b0c2a
Parsa Hosseini <officer47p@gmail.com>
2020-09-04 19:39:26 +0430

    added taskstatus to Task class and modified taskmanager class too

--- lib/providers/task_manager.dart
+++ lib/providers/task_manager.dart
@@ -1,36 +1,60 @@
 import 'package:flutter/foundation.dart';
 import 'dart:math' as math;
+import '../enums.dart';
 
 class Task {
   String id;
   String title;
   String description;
   DateTime deadLine;
-  Task({this.id, this.title, this.description, this.deadLine});
+  TaskStatus taskStatus;
+  Task({this.id, this.title, this.description, this.deadLine, this.taskStatus});
 }
 
 class TaskManager extends ChangeNotifier {
-  List<Task> _tasks = [];
+  List<Task> _tasks = [
+    Task(
+      title: "HEy",
+      description: "kjdf d skfj dsjkf",
+      deadLine: DateTime.now(),
+      id: "fjdbskjfbsdjkb",
+      taskStatus: TaskStatus.Done,
+    )
+  ];
 
   List<Task> get tasks {
     return [..._tasks];
   }
 
-  void addTask({String title, String description, DateTime deadLine}) {
+  void addTask(
+      {String title,
+      String description,
+      DateTime deadLine,
+      TaskStatus taskStatus}) {
     _tasks.add(Task(
       id: (math.Random().nextDouble() * 100000).toString(),
       title: title,
       description: description,
       deadLine: deadLine,
+      taskStatus: taskStatus,
     ));
     notifyListeners();
   }
 
   void editTask(
-      {String id, String title, String description, DateTime deadLine}) {
+      {String id,
+      String title,
+      String description,
+      DateTime deadLine,
+      TaskStatus taskStatus}) {
     final index = _tasks.indexWhere((element) => element.id == id);
     _tasks[index] = Task(
-        id: id, title: title, description: description, deadLine: deadLine);
+      id: id,
+      title: title,
+      description: description,
+      deadLine: deadLine,
+      taskStatus: taskStatus,
+    );
   }
 
   Task getTaskById(String id) {
@@ -38,6 +62,10 @@ class TaskManager extends ChangeNotifier {
       (element) => element.id == id,
       orElse: () => null,
     );
+  }
+
+  List<Task> getTasksByStatus(TaskStatus taskStatus) {
+    return _tasks.where((element) => element.taskStatus == taskStatus).toList();
   }
 
   void deleteTask(String id) {
